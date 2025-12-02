6b958d4c78d028aaab5a4188de3e2c1c4cabe739
Rezeon <Rheynoair@gmail.com>
2025-12-02 23:26:18 +0700

    penambahan docker dan redis

--- utils/redis.chace.go
+++ utils/redis.chace.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -12,13 +13,32 @@ var RedisClient *redis.Client
 
 func InitRedis() {
 	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+		log.Printf("Peringatan: REDIS_ADDR tidak disetel, menggunakan default: %s", redisAddr)
+	}
+
 	RedisClient = redis.NewClient(&redis.Options{
 		Addr: redisAddr,
 	})
 
-	_, err := RedisClient.Ping(ctx).Result()
-	if err != nil {
-		log.Fatalf("Gagal terhubung ke Redis (%s): %v", redisAddr, err)
+	maxRetries := 5
+	retryDelay := 4 * time.Second
+
+	for i := 0; i < maxRetries; i++ {
+		_, err := RedisClient.Ping(ctx).Result()
+
+		if err == nil {
+			fmt.Println("Koneksi Redis berhasil.")
+			return // KELUAR DARI FUNGSI JIKA BERHASIL
+		}
+
+		log.Printf("Gagal terhubung ke Redis (Percobaan %d/%d ke %s): %v", i+1, maxRetries, redisAddr, err)
+
+		if i < maxRetries-1 {
+			time.Sleep(retryDelay)
+		}
 	}
-	fmt.Println("Koneksi Redis berhasil.")
+
+	log.Fatalf("Aplikasi mati: Gagal terhubung ke Redis setelah %d percobaan.", maxRetries)
 }
